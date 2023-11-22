1144a674c9cc5036cb75692d2beddfe36d40b7f9
Jeff7531 <jeffmkwang@gmail.com>
2023-11-21 22:42:22 -0800

    update audit log endpoint in app.py

--- audit/app.py
+++ audit/app.py
@@ -95,7 +95,7 @@ def get_request(index):
 
 
 app = connexion.FlaskApp(__name__, specification_dir='')
-app.add_api("openapi.yml", strict_validation=True, validate_responses=True, base_path="/audit_log")
+app.add_api("openapi.yml", strict_validation=True, validate_responses=True, base_path="/audit")
 if "TARGET_ENV" not in os.environ or os.environ["TARGET_ENV"] != "test":
     CORS(app.app)
     app.app.config['CORS_HEADERS'] = 'Content-Type'
