925200a7dc1f583ffa3deac8966e9f4ace34e298
Jeff7531 <jeffmkwang@gmail.com>
2023-11-28 12:52:38 -0800

    update

--- pipeline/vars/shared_pipeline.groovy
+++ pipeline/vars/shared_pipeline.groovy
@@ -30,10 +30,11 @@ def call(image) {
             }
 
             stage('Deploy'){
-                steps{
-                    when {
+                when {
                         expression { params.DEPLOY }
-                    }
+                }
+                steps {
+                    
                     sshagent(credentials: ['SSH']) {
                     
                         sh "ssh -o StrictHostKeyChecking=no -l azureuser 172.178.11.14 'cd ~/acit-3855-lab6/deployment && docker pull penpen7531/${image}:latest && docker-compose up -d'"
