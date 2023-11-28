00dbffefe13b209caf5f73d09a341a305d59625d
Jeff7531 <jeffmkwang@gmail.com>
2023-11-28 08:53:49 -0800

    update

--- pipeline/vars/pipeline.groovy
+++ pipeline/vars/pipeline.groovy
@@ -16,25 +16,25 @@ def call(imageName) {
                     sh "safety check -r ${imageName}/requirements.txt --full-report -o text --continue-on-error"
                 }
             }
-            stage('Package') {
-                steps {
-                    withCredentials([string(credentialsId: 'ShantiDockerHub', variable: 'TOKEN')]) {
-                        sh "docker login -u 'fishfinna' -p '$TOKEN' docker.io"
-                        sh "docker build -t fishfinna/${imageName}:latest ${imageName}/."
-                        sh "docker push fishfinna/${imageName}:latest"
-                    } 
-                }
-            }
-            stage("Deploy") {
-                when {
-                    expression { params.DEPLOY }
-                }
-                steps {
-                    sshagent(credentials: ['shanti-kafka-ssh']) {
-                        sh "ssh -o StrictHostKeyChecking=no azureuser@20.151.78.202 'cd ~/microservices/deployment && docker pull fishfinna/${imageName}:latest && docker-compose up -d'"
-                    }
-                }
-            }
+            // stage('Package') {
+            //     steps {
+            //         withCredentials([string(credentialsId: 'ShantiDockerHub', variable: 'TOKEN')]) {
+            //             sh "docker login -u 'fishfinna' -p '$TOKEN' docker.io"
+            //             sh "docker build -t fishfinna/${imageName}:latest ${imageName}/."
+            //             sh "docker push fishfinna/${imageName}:latest"
+            //         } 
+            //     }
+            // }
+            // stage("Deploy") {
+            //     when {
+            //         expression { params.DEPLOY }
+            //     }
+            //     steps {
+            //         sshagent(credentials: ['shanti-kafka-ssh']) {
+            //             sh "ssh -o StrictHostKeyChecking=no azureuser@20.151.78.202 'cd ~/microservices/deployment && docker pull fishfinna/${imageName}:latest && docker-compose up -d'"
+            //         }
+            //     }
+            // }
         }
     }
 }
