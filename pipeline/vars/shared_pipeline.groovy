f5a49a7459431cfac5d936fd58d2154a31534d07
Jeff7531 <jeffmkwang@gmail.com>
2023-11-28 21:15:12 -0800

    update

--- pipeline/vars/shared_pipeline.groovy
+++ pipeline/vars/shared_pipeline.groovy
@@ -1,24 +1,33 @@
 def call(image) {
     pipeline {
         agent any
+
+        // Deploy parameter
         parameters {
             booleanParam(defaultValue: false, description: 'Deploy the App', name:'DEPLOY')
         }
+
         stages {
-        
+            
+            // Checks python code with lint. Fails under score of 5
             stage('Lint') {
                 steps {
                     sh "pip install -r ${image}/requirements.txt --break-system-packages"
                     sh "pylint --fail-under=5.0 ${image}/*.py"
                 }
             }
+
+            // Scans for vulnerabilities in requirements.txt
+            // DOCS: https://pypi.org/project/safety/ 
+
             stage ('Security Check'){
                 steps{
                     sh "safety check -r ${image}/requirements.txt --full-report -o text --continue-on-error"
                     
                 }
-                
             }
+
+            // Packages image and pushes to dockerhub
             stage('Package') {
                 steps {
                     withCredentials([string(credentialsId: 'Docker-Assignment-Token', variable: 'TOKEN')]) {
@@ -30,10 +39,15 @@ def call(image) {
                 }
             }
 
+            // Deploy application from 3855 VM 
             stage('Deploy'){
+
+                // Only run when deploy param is checked
                 when {
                         expression { params.DEPLOY }
                 }
+
+                // Use SSH credentials for key
                 steps {
                     sshagent(credentials: ['SSH']) {
                         sh "ssh -o StrictHostKeyChecking=no -l azureuser 172.178.11.14 'cd ~/acit-3855-lab6/deployment && docker pull penpen7531/${image}:latest && docker-compose up -d'"
