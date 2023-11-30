fe5db6d83b25b6847351bd8c4173b31fe3c54e72
Jeff7531 <jeffmkwang@gmail.com>
2023-11-30 09:41:53 -0800

    update

--- storage/app.py
+++ storage/app.py
@@ -124,12 +124,13 @@ def process_messages():
         try:
             client = KafkaClient(hosts=hostname, socket_timeout_ms=5000, offsets_channel_socket_timeout_ms=5000)
             topic = client.topics[str.encode(app_config["events"]["topic"])]
-            logging.info('Topic Successful')
+
             # Create a consume on a consumer group, that only reads new messages
             # (uncommitted messages) when the service re-starts (i.e., it doesn't
             # read all the old messages from the history in the message queue).
-            consumer = topic.get_balanced_consumer(consumer_group=b'events',reset_offset_on_start=False, auto_commit_enabled=True, auto_commit_interval_ms=100, zookeeper_connect='http://zookeeper:2181')
-            logging.info("Connection Successful")
+            consumer = topic.get_simple_consumer(consumer_group=b'event',reset_offset_on_start=False,auto_offset_reset=OffsetType.LATEST)
+            logger.info("Connection Successful")
+
             # Break loop after connection successful
             break
 
